c84ca0024110a1780152d635a956bfd602233244
HyeonJoonJung <HyeonJoon.Jung@gmail.com>
2019-01-06 18:21:50 -0500

    Add menu panel

---
+++ Assets/Restaurant AR/Scripts/UI/UICategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICategoriesController : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown dropDown;
+
+    public void OnCategorySelect()
+    {
+        HideAllCategories();
+        ShowSelectedCategory();
+    }
+
+    private void ShowSelectedCategory()
+    {
+        transform.GetChild(dropDown.value).gameObject.SetActive(true);
+    }
+
+    private void HideAllCategories()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
