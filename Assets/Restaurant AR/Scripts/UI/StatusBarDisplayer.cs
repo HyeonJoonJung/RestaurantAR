40080b2cee38d6a47eb4d958184aca4f1e649312
HyeonJoonJung <HyeonJoon.Jung@gmail.com>
2019-01-13 19:00:10 -0500

    show status bar if notch

---
+++ Assets/Restaurant AR/Scripts/UI/StatusBarDisplayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.iOS;
+
+public class StatusBarDisplayer : MonoBehaviour
+{
+    Vector2 screen = NGUITools.screenSize;
+
+    private void Start()
+    {
+        ShowStatusBarIfXorXs();
+    }
+
+    private void ShowStatusBarIfXorXs()
+    {
+        if (IsNotchPhone())
+        {
+            StatusBarManager.Show(true);
+        }
+    }
+
+    private bool IsNotchPhone()
+    {
+        return (Device.generation != DeviceGeneration.iPhoneX) || (Device.generation != DeviceGeneration.iPhoneXR) 
+        || (Device.generation != DeviceGeneration.iPhoneXS) || (Device.generation != DeviceGeneration.iPhoneXSMax);
+    }
+}
